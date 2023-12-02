0f665a431768aceff44416b5c2d0f08ce0c1addf
Zubair Akram <zubairprogramerexpert@gmail.com>
2023-12-02 14:23:31 +0500

    all template file added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="bg-gray-600 text-white text-5xl text-center font-bold py-4">Full Stack React Blog App...</h1>
+    </>
+  )
+}
+
+export default App
